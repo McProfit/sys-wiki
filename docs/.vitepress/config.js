fe712a0ead4641392c15b64deed8003c2653d7ce
ax-hack <ax-hack@pm.me>
2023-04-04 14:22:08 +0300

    just production init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  lang: "ru-RU",
+  title: "Системный маркетинг",
+  description: "База знаний по маркетингу и продажам, простыми словами.",
+  themeConfig: {
+    nav: [
+      {
+        text: "Оглавление",
+        link: "/content",
+      },
+      {
+        text: "Содержание",
+        items: [
+          {
+            text: "Системный маркетинг",
+            link: "/content#системный-маркетинг",
+          },
+          {
+            text: "Электронная коммерция",
+            link: "/content#электронная-коммерция",
+          },
+          {
+            text: "Финансовый учёт",
+            link: "/content#финансовый-учёт",
+          },
+         {
+            text: "Прочее",
+            link: "/content#прочее",
+          },
+        ],
+      },
+      {
+        text: "McProfit",
+        link: "https://mcprofit.biz",
+      },
+    ],
+  },
+};
---
+++ docs/index.md
@@ -0,0 +1,8 @@
+---
+home: true
+heroText: Системный маркетинг
+heroImage: /img/sys.svg
+actionText: К оглавлению
+actionLink: /content
+footer:  Информация распространяется под лицензией CC BY-NC-ND | © 2023 mcprofit.biz
+---
